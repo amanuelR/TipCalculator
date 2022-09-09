d990ffcfbb08f28254fb4f6b71d83f6b21861ae6
Amanuel Reda <amanuel.reda01@student.csulb.edu>
2022-09-08 20:38:14 -0700

    Version 3: Buttonless tip calculator

--- app/src/main/java/com/example/tipcalculatorv1/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorv1/MainActivity.kt
@@ -1,56 +1,70 @@
-package com.example.tipcalculatorv1
-
+package com.android.tipcalculatorv1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculatorv1.TipCalculator
 import com.example.tipcalculatorv1.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val tipCalc = TipCalculator(0.0f,0.0f)
-    //  private lateinit var calculateButton: Button
-    lateinit var amountBill: EditText
-    lateinit var amountTip: EditText
-    lateinit var amountTipView: TextView
-    lateinit var amountTotalView: TextView
+    private val tipCalc= TipCalculator(0.0f,0.0f)
     var money = NumberFormat.getCurrencyInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //  setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonCalculator.setOnClickListener { view: View ->
-            calculate()
-        }
+
+        binding.amountBill.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) { calculate() }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {}
+        })
+
+        // Create an instance of TextWatcher for tip percentage input
+        binding.amountTipPercent.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) { calculate() }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int,before: Int, count: Int) {}
+        })
 
     }
     fun calculate() {
+        var billAmount: Float =0.0f
+        var billTip: Float = 0.0f
 
-        val  amountBill = binding.amountBill
-        val stringBillAmount = amountBill.text.toString()
-        val billAmount = stringBillAmount.toFloat()
-        tipCalc.bill = billAmount
+        // get the input for bill from layout
+        val  amountBill1 = binding.amountBill
+        val stringBillAmount = amountBill1.text.toString()
 
-        //Get amount of tip percentage
+        //validate bill amount input
+        billAmount = if (stringBillAmount.isEmpty()) 0.0f
+        else stringBillAmount.toFloat()
+        tipCalc.bill=billAmount
 
-        val billTip = binding.amountTipPercent.text.toString().toFloat()
-        tipCalc.tip=.01f * billTip
+        // get the input for tip from layout
+        val amountTip1 = binding.amountTipPercent
+        val amountTipInput = amountTip1.text.toString()
 
-        //Determine the amount of tip and convert it to string
-        val amountTip: String = money.format(tipCalc.tipAmount()).toString()
+        // validate bill tip input
+        billTip = if (amountTipInput.isEmpty()) 0.0f
+        else amountTipInput.toFloat()
+        tipCalc.tip = billTip * 0.01f
 
-        //Determine the total and convert it to string
-        val amountTotal: String = money.format(tipCalc.totalAmount()).toString()
+        // covert the amounts to money format
+        val amountTip: String = money.format(tipCalc.tipAmount().toString())
+        val amountTotal: String = money.format(tipCalc.totalAmount().toString())
 
-        //Display the amount of tip and convert it to string
-        binding.amountTip.setText(amountTip)
-        binding.amountTotal.setText(amountTotal)
+        // display the results in the layout text view for tip and total amount
+        binding.amountTotal.text = amountTotal
+        binding.amountTip.text = amountTip
 
     }
 }
